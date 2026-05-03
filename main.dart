60f99cc9f4aae82024efc90fb346da5dade68aa0
Mahim123-Hassan <hmahim951@gmail.com>
2026-05-03 23:30:56 +0600

    first commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+
+void main (){
+  int number=-5;
+  if(number<0){
+    print("Negative Number");
+  }else if(number==0){
+    print("Zero");
+  }else if(number %2==0){
+    print("Even Number");
+  }else{
+    print("Odd Number");
+  }
+}
