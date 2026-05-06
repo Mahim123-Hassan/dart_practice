6556f59b428c6565c593e6d32e56e654aa28ef67
Mahim123-Hassan <hmahim951@gmail.com>
2026-05-06 15:49:27 +0600

    aded task 2

--- main.dart
+++ main.dart
@@ -1,28 +1,11 @@
 
-void main(){
-  List<String>Mylist=['A','B',"c",'d','E','F'];
-  print(Mylist);
-  print(Mylist.length);
-  Mylist.insert(0, 'Mahim');
-  print(Mylist);
-  Mylist.add('Siam');
-  print(Mylist);
-  Mylist.addAll(['Masum','Sagor','Joynal']);
-  print(Mylist);
-  Mylist[1]='Shahin';
-  print(Mylist);
-  Mylist.replaceRange(0, 2, ['ma','baba']);
-  print(Mylist);
-  print(Mylist.isEmpty);
-  print(Mylist.isNotEmpty);
-  print(Mylist.first);
-  print(Mylist.last);
-  Mylist.remove('baba');
-  print(Mylist);
-  Mylist.removeAt(2);
-  print(Mylist);
-  Mylist.clear();
-  print(Mylist);
 
-
+void main (){
+  List <String> items=['Rice','Oil','Salt'];
+  print(items);
+  items.add('Sagor');
+  print(items);
+  items.remove('Oil');
+  print(items);
+  print(items.contains('Salt'));
 }
