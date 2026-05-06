7db536e8b8ddfb72bca902edacb034864c46b1ef
Mahim123-Hassan <hmahim951@gmail.com>
2026-05-06 15:08:58 +0600

    added Remove

--- main.dart
+++ main.dart
@@ -13,5 +13,16 @@ void main(){
   print(Mylist);
   Mylist.replaceRange(0, 2, ['ma','baba']);
   print(Mylist);
+  print(Mylist.isEmpty);
+  print(Mylist.isNotEmpty);
+  print(Mylist.first);
+  print(Mylist.last);
+  Mylist.remove('baba');
+  print(Mylist);
+  Mylist.removeAt(2);
+  print(Mylist);
+  Mylist.clear();
+  print(Mylist);
+
 
 }
